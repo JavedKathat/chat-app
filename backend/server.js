7ef6fc6aa6a2e6f4559716ef7ceb5e18831ed0ad
Javed Kathat <javedkhankathat3040@gmail.com>
2024-09-16 23:49:39 +0530

    First part completed - Backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import { app, server } from "./socket/socket.js";
+import mongodbConnect from "./db/mongodbConnect.js";
+
+dotenv.config();
+
+// const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   //root route  http://localhost:5000/
+//   res.send("Hello World!");
+// });
+
+server.listen(PORT, () => {
+  mongodbConnect();
+  console.log(`listening on port ${PORT}`);
+});
