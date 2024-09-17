88cc44fd43cd11064fba4c476592698bfe13355a
Javed Kathat <javedkhankathat3040@gmail.com>
2024-09-18 00:55:47 +0530

    12,55

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      // const socket = io("https://chat-app-yt.onrender.com", {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-9tfk.onrender.com", {
         query: {
           userId: authUser._id,
         },
